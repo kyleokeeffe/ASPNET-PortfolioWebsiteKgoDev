fe76238acffd30a68e6027f28f3714106d393747
kyleokeeffe <kokeeffe@my.centennialcollege.ca>
2023-09-13 23:31:19 -0400

    Updated data model, added new migration

--- Program.cs
+++ Program.cs
@@ -74,8 +74,8 @@ namespace PortfolioWebsiteKgoDev
         public string Type { get; set; }
         public string Language { get; set; }
         public string Description { get; set; }
-
-
+        public string Photo { get; set; }
+        public string Link { get; set; }
     }
     public class ProjectDbContext : Microsoft.EntityFrameworkCore.DbContext
     {
