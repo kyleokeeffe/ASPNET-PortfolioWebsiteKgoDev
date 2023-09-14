63e0c73a0d2766bebabb6a789a856d1339e21560
kyleokeeffe <kokeeffe@my.centennialcollege.ca>
2023-09-13 23:47:59 -0400

    Added about html content, updated nav-menu

--- ClientApp/src/app/pages/my-services/my-services.component.ts
+++ ClientApp/src/app/pages/my-services/my-services.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-services.component.css']
 })
 export class MyServicesComponent {
+  webDevDiv: boolean = false;
+  standProgDiv: boolean = false;
+  dbAdminDiv: boolean = false;
+  resHostDiv: boolean = false;
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  ShowWebDev() {
+    this.webDevDiv = true;
+    this.standProgDiv = false;
+    this.dbAdminDiv = false;
+    this.resHostDiv = false;
+  }
+  ShowStandProg() {
+    this.webDevDiv = false;
+    this.standProgDiv = true;
+    this.dbAdminDiv = false;
+    this.resHostDiv = false;
+  }
+  ShowDbAdmin() {
+    this.webDevDiv = false;
+    this.standProgDiv = false;
+    this.dbAdminDiv = true;
+    this.resHostDiv = false;
+  }
+  ShowResHost() {
+    this.webDevDiv = false;
+    this.standProgDiv = false;
+    this.dbAdminDiv = false;
+    this.resHostDiv = true;
+  }
 }
