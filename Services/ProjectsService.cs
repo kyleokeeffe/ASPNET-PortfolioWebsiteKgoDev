0e7a216120f60fbe7de4ecb97f4b8a45fd45a04d
kyleokeeffe <kokeeffe@my.centennialcollege.ca>
2023-10-14 12:58:37 -0400

    Updated ProjectsService with MongoDB Linq provider

--- Services/ProjectsService.cs
+++ Services/ProjectsService.cs
@@ -2,17 +2,23 @@
 using Microsoft.Extensions.Options;
 using MongoDB.Bson.IO;
 using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 
 namespace KgoDevBackend.Services
 {
     public class ProjectsService
     {
         private readonly IMongoCollection<Project> _projectsCollection;
-        public ProjectsService(
-            IOptions<PortfolioDatabaseSettings> portfolioDatabaseSettings)
+        public ProjectsService(IOptions<PortfolioDatabaseSettings> portfolioDatabaseSettings)
         {
-            var mongoClient = new MongoClient(
-                portfolioDatabaseSettings.Value.ConnectionString);
+            
+            MongoClientSettings settings = MongoClientSettings.FromConnectionString(portfolioDatabaseSettings.Value.ConnectionString);
+            
+            settings.LinqProvider = LinqProvider.V3;
+
+            //var mongoClient = new MongoClient(portfolioDatabaseSettings.Value.ConnectionString);
+            var mongoClient = new MongoClient(settings);
+            
             var mongoDatabase = mongoClient.GetDatabase(
                 portfolioDatabaseSettings.Value.DatabaseName);
 
@@ -23,12 +29,22 @@ namespace KgoDevBackend.Services
 
         public async Task<List<Project>> GetAsync() =>
             await _projectsCollection.Find(_ => true).ToListAsync();
+
         public async Task<Project?> GetAsync(string id) =>
-            await _projectsCollection.Find(x=> x.Id == id).FirstOrDefaultAsync();
+            await _projectsCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        //public async Task<Project?> GetAsync(string filterType, string filterLang) {
+        //   var query = from project in _projectsCollection 
+        //    }
+            
+        //    await _projectsCollection.
+
         public async Task CreateAsync(Project newProject) =>
             await _projectsCollection.InsertOneAsync(newProject);
+
         public async Task UpdateAsync(string id, Project updatedProject) =>
             await _projectsCollection.ReplaceOneAsync(x=> x.Id == id, updatedProject);
+
         public async Task RemoveAsync(string id) =>
             await _projectsCollection.DeleteOneAsync(x=> x.Id == id);
     }
